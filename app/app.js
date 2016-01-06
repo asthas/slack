570ca25b45c4ea4c0639f48756114aaa0a9ef6b1
Swapnil Raj <sr@sixth.io>
2016-01-06 17:53:08 +0530

    Working channels

--- app/app.js
+++ app/app.js
@@ -18,16 +18,104 @@ angular
     $stateProvider
       .state('home', {
         url: '/',
-        templateUrl: 'home/home.html'
+        templateUrl: 'home/home.html',
+        resolve: {
+          requireNoAuth: function($state, Auth){
+            console.log('Invoked home\nState:', $state);
+            return Auth.$requireAuth().then(function(auth){
+              console.log('At home\nAuth:', auth);
+              window.mullu = $state;
+              $state.go('channels');
+            }, function(error){
+                console.log('Home locked:', error);
+                return;
+            });
+          }
+        }
       })
       .state('login', {
         url: '/login',
-        templateUrl: 'auth/login.html'
+        templateUrl: 'auth/login.html',
+        controller: 'AuthCtrl as authCtrl',
+        resolve: {
+          requireNoAuth: function($state, Auth){
+            return Auth.$requireAuth().then(function(auth){
+              $state.go('home');
+            }, function(error){
+                return;
+            });
+          }
+        }
       })
       .state('register', {
         url: '/register',
-        templateUrl: 'auth/register.html'
-      });
+        templateUrl: 'auth/register.html',
+        controller: 'AuthCtrl as authCtrl',
+        resolve: {
+          requireNoAuth: function($state, Auth) {
+            console.log('requireNoAuth');
+            return Auth.$requireAuth().then(function(auth){
+              console.log('Navigating home');
+              $state.go('home');
+            }, function(error){
+                console.log('Error', error);
+                return;
+            });
+          }
+        }
+      })
+      .state('profile', {
+          url:'/profile',
+          controller: 'ProfileCtrl as profileCtrl',
+          templateUrl: 'users/profile.html',
+          resolve: {
+            auth: function($state, Users, Auth){
+              return Auth.$requireAuth().catch(function(){
+                $state.go('home');
+              });
+            },
+            profile: function(Users, Auth){
+              return Auth.$requireAuth().then(function(auth){
+                return Users.getProfile(auth.uid).$loaded();
+              });
+            }
+          }
+        })
+        .state('channels', {
+          url: '/channels',
+          controller: 'ChannelsCtrl as channelsCtrl',
+          templateUrl: 'channels/index.html',
+          resolve: {
+            channels: function(Channels){
+              return Channels.$loaded();
+            },
+          
+            profile: function($state, Auth, Users){
+              console.log('State:', $state, '\nUsers:', Users, '\nAuth:', Auth.$requireAuth);
+              return Auth.$requireAuth().then(function(auth){
+                console.log("SEETHIS:", auth);
+                return Users.getProfile(auth.uid).$loaded().then(function(profile){
+                  console.log("WOAH");
+                  if(profile.displayName){
+                    console.log('If worked', profile);
+                    return profile;
+                  } else {
+                    console.log('Else :)))', profile);
+                    $state.go('profile');
+                  }
+                });
+              }, function(error){
+                console.log('ERROR: For Auth.requireAuth promise', error);
+                $state.go('home');
+              });
+            }
+          }
+        })
+        .state('channels.create', {
+          url:'/create',
+          templateUrl: 'channels/create.html',
+          controller: 'ChannelsCtrl as channelsCtrl'
+        })
 
     $urlRouterProvider.otherwise('/');
   })
---
+++ app/channels/channels.service.js
@@ -0,0 +1,8 @@
+angular.module('angularfireSlackApp')
+	.factory('Channels', function($firebaseArray, FirebaseUrl){
+		var ref = new Firebase(FirebaseUrl+'channels');
+		console.log(ref, FirebaseUrl);
+		var channels = $firebaseArray(ref);
+
+		return channels;
+	});
