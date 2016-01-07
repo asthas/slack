0ba801d51e2b287136e2f6dd2c08d1cd1465ea2e
Swapnil Raj <sr@sixth.io>
2016-01-07 11:53:08 +0530

    Added message functionality

--- app/app.js
+++ app/app.js
@@ -116,6 +116,19 @@ angular
           templateUrl: 'channels/create.html',
           controller: 'ChannelsCtrl as channelsCtrl'
         })
+        .state('channels.messages', {
+          url:'/{channelId}/messages',
+          templateUrl: 'channels/messages.html',
+          controller: 'MessagesCtrl as messagesCtrl',
+          resolve: {
+            messages: function($stateParams, Messages) {
+              return Messages.forChannel($stateParams.channelId).$loaded();
+            }, 
+            channelName: function($stateParams, channels) {
+              return '#' +channels.$getRecord($stateParams.channelId).name;
+            }
+          }
+        })
 
     $urlRouterProvider.otherwise('/');
   })
--- app/channels/channels.service.js
+++ app/channels/channels.service.js
@@ -6,3 +6,4 @@ angular.module('angularfireSlackApp')
 
 		return channels;
 	});
+
